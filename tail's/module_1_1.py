ebe396c3a12acae41a496fe74a878ebc3ee3229d
Krochi <77779207+Krochi@users.noreply.github.com>
2024-12-14 08:52:27 +0300

    Add files via upload

---
+++ tail's/module_1_1.py
@@ -0,0 +1,26 @@
+# 1st program
+
+res_1 = (9 ** 0.5) * 5
+print(res_1)
+
+# 2nd program
+
+cond = (9.99 > 9.98) and (1000 != 1000.1)
+print(cond)
+
+# 3rd program
+
+my_priority = 2 * 2 + 2
+my__priority = 2 * (2 + 2)
+comparison = my_priority == my_priority
+print(my_priority)
+print(my_priority)
+print(comparison)
+
+# 4th program
+
+num_str = '123.456'
+num = float(num_str)
+sh_number = num * 10
+first = int(sh_number) % 10
+print(first)
